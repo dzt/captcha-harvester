c46e5ded968fd6b563ab839dad9b11c243b8ab1a
Peter Soboyejo <thepcmrtim@gmail.com>
2018-01-07 02:53:56 -0500

    win installer

---
+++ scripts/installer.js
@@ -0,0 +1,33 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+const rimraf = require('rimraf')
+
+deleteOutputFolder()
+  .then(getInstallerConfig)
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.log('err')
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  const rootPath = path.join(__dirname, '..')
+  const outPath = path.join(rootPath, 'dist')
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'win-unpacked'),
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    setupExe: 'CaptchaHarvesterSetup.exe',
+    setupIcon: path.join(rootPath, 'logo.ico'),
+    skipUpdateIcon: true
+  })
+}
+
+function deleteOutputFolder () {
+  return new Promise((resolve, reject) => {
+    rimraf(path.join(__dirname, '..', 'out', 'windows-installer'), (error) => {
+      error ? reject(error) : resolve()
+    })
+  })
+}
